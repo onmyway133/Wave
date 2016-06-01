b58e8959907fb9bb3db70abe231616617c73dac5
Khoa Pham <onmyway133@gmail.com>
2016-06-01 21:45:45 +0200

    Setup demo

---
+++ Example/WaveDemo/WaveDemo/Sources/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  WaveDemo
+//
+//  Created by Khoa Pham on 01/06/16.
+//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
+
+public struct Item {
+  let name: String
+  let action: () -> Void
+}
--- Example/WaveDemo/WaveDemo/Sources/ViewController.swift
+++ Example/WaveDemo/WaveDemo/Sources/ViewController.swift
@@ -3,7 +3,108 @@ import Wave
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var box1: UIView!
+  @IBOutlet weak var box2: UIView!
+  @IBOutlet weak var box3: UIView!
+
+  var items: [Item] = []
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    box3.layer.cornerRadius = box3.frame.width/2
+
+    setup()
+  }
+
+  @IBAction func reset(sender: UIButton) {
+    
+  }
+
+  func setup() {
+    let box1 = self.box1
+    let box2 = self.box2
+    let box3 = self.box3
+
+    // Ride
+
+    items.append(Item(name: "flipX", action: {
+      box1.wave.flipX()
+    }))
+
+    items.append(Item(name: "flipY", action: {
+      box1.wave.flipY()
+    }))
+
+    items.append(Item(name: "flash", action: {
+      box1.wave.flash()
+    }))
+
+    items.append(Item(name: "shake", action: {
+      box1.wave.shake()
+    }))
+
+    items.append(Item(name: "pop", action: {
+      box1.wave.pop()
+    }))
+
+    items.append(Item(name: "morph", action: {
+      box1.wave.morph()
+    }))
+
+    items.append(Item(name: "squeeze", action: {
+      box1.wave.squeeze()
+    }))
+
+    items.append(Item(name: "wobble", action: {
+      box1.wave.wobble()
+    }))
+
+    items.append(Item(name: "swing", action: {
+      box1.wave.swing()
+    }))
+
+    // View
+
+    items.append(Item(name: "view 1", action: {
+        Chain<View.BasicAnimation>()
+        .newAction()
+        .view(box1)
+        .rotate(M_PI_2)
+        .newAction()
+        .block {
+          box2.frame.origin.y -= 40
+          box3.frame.origin.y += 10
+        }
+        .run()
+    }))
+
+    // Layer
+
+    tableView.reloadData()
+  }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return items.count
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let item = items[indexPath.row]
+
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    cell.textLabel?.text = item.name
+
+    return cell
+  }
+
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    
+    let item = items[indexPath.row]
+    item.action()
   }
 }
