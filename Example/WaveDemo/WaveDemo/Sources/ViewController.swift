7d6a590b8859bc59697b5331e9707f6deb005852
Khoa Pham <onmyway133@gmail.com>
2017-07-28 13:19:28 +0200

    Comment old code for now

--- Example/WaveDemo/WaveDemo/Sources/ViewController.swift
+++ Example/WaveDemo/WaveDemo/Sources/ViewController.swift
@@ -31,148 +31,129 @@ class ViewController: UIViewController {
     // Ride
 
     items.append(Item(name: "flipX", action: {
-      box1.wave(action: )
-      box1.wave.flipX().run()
+//      box1.wave.flipX().run()
     }))
 
     items.append(Item(name: "flipY", action: {
-      box1.wave.flipY().run()
+//      box1.wave.flipY().run()
     }))
 
     items.append(Item(name: "flash", action: {
-      box1.wave.flash().run()
+//      box1.wave.flash().run()
     }))
 
     items.append(Item(name: "shake", action: {
-      box1.wave.shake().run()
+//      box1.wave.shake().run()
     }))
 
     items.append(Item(name: "pop", action: {
-      box1.wave.pop().run()
+//      box1.wave.pop().run()
     }))
 
     items.append(Item(name: "morph", action: {
-      box1.wave.morph().run()
+//      box1.wave.morph().run()
     }))
 
     items.append(Item(name: "squeeze", action: {
-      box1.wave.squeeze().run()
+//      box1.wave.squeeze().run()
     }))
 
     items.append(Item(name: "wobble", action: {
-      box1.wave.wobble().run()
+//      box1.wave.wobble().run()
     }))
 
     items.append(Item(name: "swing", action: {
-      box1.wave.swing().run()
+//      box1.wave.swing().run()
     }))
 
     // View
 
     items.append(Item(name: "view move change color", action: {
-      Chain<View.Action>()
-        .view(box2)
-        .add(ViewBasicAnimation().fadeOut().moveX(20))
-        .add(ViewBasicAnimation().changeBackground(UIColor.blueColor()))
-        .run()
+//      Chain<View.Action>()
+//        .view(box2)
+//        .add(ViewBasicAnimation().fadeOut().moveX(20))
+//        .add(ViewBasicAnimation().changeBackground(UIColor.blueColor()))
+//        .run()
     }))
 
     items.append(Item(name: "blink", action: {
-      Chain<View.Action>()
-        .add(ViewBasicAnimation().view(box1).fadeOut())
-        .add(ViewBasicAnimation().view(box2).fadeOut())
-        .add(ViewBasicAnimation().view(box3).fadeOut())
-        .then()
-        .add(ViewBasicAnimation().view(box1).fadeIn())
-        .add(ViewBasicAnimation().view(box2).fadeIn())
-        .add(ViewBasicAnimation().view(box3).fadeIn())
-        .run()
+//      Chain<View.Action>()
+//        .add(ViewBasicAnimation().view(box1).fadeOut())
+//        .add(ViewBasicAnimation().view(box2).fadeOut())
+//        .add(ViewBasicAnimation().view(box3).fadeOut())
+//        .then()
+//        .add(ViewBasicAnimation().view(box1).fadeIn())
+//        .add(ViewBasicAnimation().view(box2).fadeIn())
+//        .add(ViewBasicAnimation().view(box3).fadeIn())
+//        .run()
     }))
 
     items.append(Item(name: "wait log", action: {
-      Chain<View.Action>()
-      .add(ViewBasicAnimation().view(box1).fadeOut())
-      .thenLog("done fade out")
-      .thenWait(1)
-      .then()
-      .add(ViewBasicAnimation().view(box1).fadeIn())
-      .thenLog("done fade in")
-      .run()
+//      Chain<View.Action>()
+//      .add(ViewBasicAnimation().view(box1).fadeOut())
+//      .thenLog("done fade out")
+//      .thenWait(1)
+//      .then()
+//      .add(ViewBasicAnimation().view(box1).fadeIn())
+//      .thenLog("done fade in")
+//      .run()
     }))
 
     items.append(Item(name: "view rotate move move", action: {
-      Chain<View.Action>()
-      .add(ViewBasicAnimation()
-        .view(box3)
-        .moveY(100)
-        .delay(2)
-        .duration(3)
-        .repeatCount(2)
-        .options([UIViewAnimationOptions.CurveEaseIn]))
-      .then()
-      .add(ViewBasicAnimation().view(box2).moveX(-10))
-      .then()
-      .add(ViewSpringAnimation()
-        .block {
-          self.counter = (self.counter + 1) % 4
-          box1.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_4) * CGFloat(self.counter))
-        }
-      )
-      .run()
+//      Chain<View.Action>()
+//      .add(ViewBasicAnimation()
+//        .view(box3)
+//        .moveY(100)
+//        .delay(2)
+//        .duration(3)
+//        .repeatCount(2)
+//        .options([UIViewAnimationOptions.CurveEaseIn]))
+//      .then()
+//      .add(ViewBasicAnimation().view(box2).moveX(-10))
+//      .then()
+//      .add(ViewSpringAnimation()
+//        .block {
+//          self.counter = (self.counter + 1) % 4
+//          box1.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_4) * CGFloat(self.counter))
+//        }
+//      )
+//      .run()
     }))
 
     items.append(Item(name: "layer then view", action: {
-      box1.wave.swing()
-      .then(Chain<View.Action>())
-      .add(ViewBasicAnimation().view(box1).moveX(10))
-      .then()
-      .add(ViewBasicAnimation().view(box1).moveY(10))
-      .run()
+//      box1.wave.swing()
+//      .then(Chain<View.Action>())
+//      .add(ViewBasicAnimation().view(box1).moveX(10))
+//      .then()
+//      .add(ViewBasicAnimation().view(box1).moveY(10))
+//      .run()
     }))
 
     // Layer
 
     items.append(Item(name: "layer path", action: {
-      let path = UIBezierPath(arcCenter: box2.center, radius: 100, startAngle: 10, endAngle: 100, clockwise: true)
-
-      Chain<Layer.Action>()
-      .layer(box2.layer)
-      .add(LayerKeyframeAnimation()
-        .keyPath("position")
-        .calculationMode(Layer.CalculationMode.Paced)
-        .fillMode(Layer.FillMode.Forwards)
-        .duration(3)
-        .path(path)
-      )
-      .run()
+//      let path = UIBezierPath(arcCenter: box2.center, radius: 100, startAngle: 10, endAngle: 100, clockwise: true)
+//
+//      Chain<Layer.Action>()
+//      .layer(box2.layer)
+//      .add(LayerKeyframeAnimation()
+//        .keyPath("position")
+//        .calculationMode(Layer.CalculationMode.Paced)
+//        .fillMode(Layer.FillMode.Forwards)
+//        .duration(3)
+//        .path(path)
+//      )
+//      .run()
 
     }))
 
     items.append(Item(name: "layer morph and flash", action: {
-      box1.wave.morph()
-      .then(box1.wave.flash())
-      .run()
+//      box1.wave.morph()
+//      .then(box1.wave.flash())
+//      .run()
     }))
 
     tableView.reloadData()
-
-    let chain = Chain<Layer.Action>()
-
-    var wait = Wait()
-    wait.interval = 2
-
-    var custom = Custom()
-    custom.block = {
-      print("wave")
-    }
-
-    chain
-    .then()
-    .add(LayerSpringAnimation())
-    .then(Chain<View.Action>())
-    .add(ViewKeyframeAnimation())
-
-    chain.then(wait).then(custom)
   }
 }
 
