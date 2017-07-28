362b0093012a1e30850fb5f57dcaba6724e6993a
Khoa Pham <onmyway133@gmail.com>
2017-07-28 13:29:56 +0200

    Add layer and animationMaker

--- Sources/Action.swift
+++ Sources/Action.swift
@@ -44,6 +44,24 @@ public extension Action {
 }
 
 public extension Action {
+  public static func layer(animationMaker: @escaping () -> CAAnimation) -> Action {
+    return Action(animation: { view, completion in
+      CATransaction.begin()
+      let animation = animationMaker()
+
+      // Callback function
+      CATransaction.setCompletionBlock {
+        completion()
+      }
+
+      // Do the actual animation and commit the transaction
+      view.layer.add(animation, forKey: "")
+      CATransaction.commit()
+    })
+  }
+}
+
+public extension Action {
   public static func move() -> Action {
     return Action(animation: { view, completion in
       UIView.animate(withDuration: 2, animations: {
@@ -69,26 +87,19 @@ public extension Action {
   }
 
   public static func flipX() -> Action {
-    return Action(animation: { view, completion in
+    return .layer(animationMaker: {
       var perspective = CATransform3DIdentity
       perspective.m34 = 1.0 / -500
 
       CATransaction.begin()
       let animation = CABasicAnimation(keyPath: "transform")
-      animation.duration = 0.5
+      animation.duration = 0.25
       animation.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
       animation.toValue = NSValue(caTransform3D:
         CATransform3DConcat(perspective, CATransform3DMakeRotation(CGFloat.pi, 0, 1, 0)))
       animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
 
-      // Callback function
-      CATransaction.setCompletionBlock {
-        completion()
-      }
-
-      // Do the actual animation and commit the transaction
-      view.layer.add(animation, forKey: #function)
-      CATransaction.commit()
+      return animation
     })
   }
 }
